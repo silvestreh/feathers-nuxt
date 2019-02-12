804ab9f6cc8888950499e1034f682b0ee97b06c2
Silvestre Herrera <silvestre.herrera@gmail.com>
2019-02-12 14:34:23 -0300

    Add some meta and styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,19 @@
 const { resolve } = require('path');
+const package = require('./package.json');
 
 module.exports = {
   dev: process.env.NODE_ENV !== 'production',
   srcDir: resolve(__dirname, './client'),
-  router: { middleware: ['auth'] }
+  router: { middleware: ['auth'] },
+  head: {
+    title: `${package.name} — ${package.description}`,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ]
+  },
+  css: [
+    'normalize.css/normalize.css',
+    './client/styles.css'
+  ]
 };
