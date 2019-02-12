324a3054c47b75164f22bb69ab0f6f1d82b3fd85
Silvestre Herrera <silvestre.herrera@gmail.com>
2019-02-12 14:35:08 -0300

    Rewrite nuxt middleware to accept JSON

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,12 @@
-const nuxt = require('./nuxt');
-
+const { render } = require('./nuxt');
 const isTestEnv = process.env.NODE_ENV === 'test';
 
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
-  if (!isTestEnv) nuxt(app);
+  app.use((req, res, next) => {
+    const accepts = req.accepts('html', 'json');
+    if (accepts === 'json') return next();
+    if (!isTestEnv) render(req, res, next);
+  });
 };
--- src/middleware/nuxt.js
+++ src/middleware/nuxt.js
@@ -1,13 +1,11 @@
 const { Nuxt, Builder } = require('nuxt');
 const nuxtConfig = require('../../nuxt.config');
 
-module.exports = function (app) {
-  const nuxt = new Nuxt(nuxtConfig);
+const nuxt = new Nuxt(nuxtConfig);
 
-  if (nuxtConfig.dev) {
-    const builder = new Builder(nuxt);
-    builder.build();
-  }
+if (nuxtConfig.dev) {
+  const builder = new Builder(nuxt);
+  builder.build();
+}
 
-  app.use(nuxt.render);
-};
+module.exports = nuxt;
