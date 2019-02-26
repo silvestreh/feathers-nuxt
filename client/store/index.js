abef88a9259db20fd3e00f3ab3ab19808858c82d
Silvestre Herrera <silvestre.herrera@gmail.com>
2019-02-26 19:50:01 -0300

    Use module mode for nuxt store

--- client/store/index.js
+++ client/store/index.js
@@ -1,31 +1,27 @@
-import Vuex from 'vuex';
 import feathersClient from '../feathers-client';
 import feathersVuex, { initAuth } from 'feathers-vuex';
 
 const { service, auth } = feathersVuex(feathersClient, { idField: '_id' });
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {
-      nuxtServerInit({ commit, dispatch }, { req }) {
-        return initAuth({
-          commit,
-          dispatch,
-          req,
-          moduleName: 'auth',
-          cookieName: 'feathers-jwt'
-        })
-      }
-    },
-    plugins: [
-      service('users'),
-      auth({
-        state: { publicPages: ['index', 'authenticate'] },
-        userService: 'users'
-      })
-    ]
+export const state = () => ({});
+export const mutations = {};
+
+export const actions = {
+  nuxtServerInit({ commit, dispatch }, { req }) {
+    return initAuth({
+      commit,
+      dispatch,
+      req,
+      moduleName: 'auth',
+      cookieName: 'feathers-jwt'
+    })
+  }
+};
+
+export const plugins = [
+  service('users'),
+  auth({
+    state: { publicPages: ['index', 'authenticate'] },
+    userService: 'users'
   })
-}
-export default createStore
+];
