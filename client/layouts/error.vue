1aaecb944f54c287b8724d9556971968bc3345b7
Ryan Wheale <ryan.wheale@gmail.com>
2021-02-16 16:41:22 -0700

    chore: create layouts folder, move static assets from feathers to nuxt

---
+++ client/layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </main>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+  }
+</script>
--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const favicon = require('serve-favicon');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -32,9 +31,7 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
-// Host the public folder
-app.use('/', express.static(app.get('public')));
+// NOTE: all static assets are served from the nuxt static folder (including favicon.ico)
 
 // Set up Plugins and providers
 app.configure(express.rest());
